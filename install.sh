6fcc7b8c416965721819ebdf5dd4e6d6407061ee
ANCELIN Julien <julien.ancelin@inra.fr>
2020-01-22 16:23:07 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#-----------------------------------------------------------------------------
+##installation of rtklib for android userland debian (test with ublox zed-F9P)
+##ssh rover@192.168.43.1 -p2022   > smartphone
+##ssh rover@172.24.1.62 -p2022    > raspberry
+#-----------------------------------------------------------------------------
+##install tools
+sudo apt-get update
+sudo apt-get install -y gcc git build-essential automake \
+                        wget zip unzip nano psmisc procps
+##install rtklib
+git clone -b demo5 https://github.com/rtklibexplorer/RTKLIB.git
+cd ./RTKLIB/app 
+make all 
+make install
+cp ./str2str/gcc/str2str /bin 
+make clean
+cd
+
+##add start.sh to ./
+#wget --no-check-certificate -P ./ http://
+chmod +x start.sh
+##lien symbolique pour l'execution au lancement de la session (pas possible au demarrage avec userland)
+ln -i start.sh /etc/profile.d/start.sh
+
+#rtcrvc conf
+#wget --no-check-certificate -P /storage/internal/gnss http://
+
+
