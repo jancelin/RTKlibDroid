1394c54b26016c3448ac2625c1ce143006ec833f
jancelin <julien.ancelin@inra.fr>
2020-01-28 11:46:59 +0100

    upd install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #-----------------------------------------------------------------------------
 ##installation of rtklib for android userland debian (test with ublox zed-F9P)
-##ssh rover@192.168.43.1 -p2022   > smartphone
+##ssh rover@192.168.43.1 -p2022   > smartphone wifi hotspot
 ##ssh rover@172.24.1.62 -p2022    > raspberry
 ### Password: 12345678
 #-----------------------------------------------------------------------------
@@ -14,7 +14,6 @@ git clone -b demo5 https://github.com/rtklibexplorer/RTKLIB.git
 cd ./RTKLIB/app 
 make all 
 make install
-#cp ./str2str/gcc/str2str /bin 
 make clean
 cd
 
@@ -24,6 +23,10 @@ chmod +x start.sh
 ##symbolic link for execution at start of session (not possible at start with userland)
 ln -i start.sh /etc/profile.d/start.sh
 
-#rtkrcv config
+#create storage solutions for gnss llh logs >sol_%Y%m%d%h%M%S.pos
+mkdir /storage/internal/gnss
+mkdir /storage/internal/gnss/solutions
+
+#import rtkrcv config
 wget --no-check-certificate -P /storage/internal/gnss https://raw.githubusercontent.com/jancelin/RTKlibDroid/master/rtkrcv.txt
 wget --no-check-certificate -P /storage/internal/gnss https://raw.githubusercontent.com/jancelin/RTKlibDroid/master/ZED-F9P.cmd
