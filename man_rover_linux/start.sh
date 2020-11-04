0093b13fd8b93866b06927f4abbf7136e305113b
ANCELIN Julien <julien.ancelin@inra.fr>
2020-11-04 12:03:46 +0100

    Update start.sh

--- man_rover_linux/start.sh
+++ man_rover_linux/start.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 #RUN
-echo '#######################################
+echo '#######################################'
 echo 'Start rtkrcv '
 echo '* display solution with : status 1'
 echo '* close server with: shutdown'
 echo '* all command : help'
-echo '#######################################
+echo '* modify RTKlib param: sudo nano ./RTKroverConf/rtkrcv_use.txt'
+echo '* modify F9P param:    sudo nano ./RTKroverConf/F9P_use.cmd'
+echo '#######################################'
 
 ## Run RTKlib service
 if pgrep rtkrcv
- then pkill rtkrcv && rtkrcv -s -o ./run/rtkrcv_use.txt
- else rtkrcv -s -o ./run/rtkrcv_use.txt
+ then pkill rtkrcv && rtkrcv -s -o ./RTKroverConf/rtkrcv_use.txt
+ else rtkrcv -s -o ./RTKroverConf/rtkrcv_use.txt
 fi
