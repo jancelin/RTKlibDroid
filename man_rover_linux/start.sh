89b51617a4b825b810e198a80ade32f1111bf47e
ANCELIN Julien <julien.ancelin@inra.fr>
2020-11-04 11:57:07 +0100

    Update start.sh

--- man_rover_linux/start.sh
+++ man_rover_linux/start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#RUN
+echo '#######################################
+echo 'Start rtkrcv '
+echo '* display solution with : status 1'
+echo '* close server with: shutdown'
+echo '* all command : help'
+echo '#######################################
+
 ## Run RTKlib service
 if pgrep rtkrcv
  then pkill rtkrcv && rtkrcv -s -o ./run/rtkrcv_use.txt
